4631c271b652ddaa9777a9adc5427cbfef039086
Clonerclo <129645851+Clonerclo@users.noreply.github.com>
2025-08-09 16:22:28 -0700

    loop is broken

--- TTS.py
+++ TTS.py
@@ -5,6 +5,14 @@ engine = pyttsx3.init()
 engine.setProperty('rate', int(input("Please enter the wpm you would like me to speak at (150 is semi-slow): ")))
 engine.setProperty('volume', float(input("Volume level (0.0 to 1.0): ")))
 
+# yap ff alle voices available
+
+voices = engine.getProperty('voices')
+
+for i, voice in enumerate(voices):
+    print(f"\033[92mVoice {i}: {voice.name}, {voice.gender}\033[0m")
+
+sel_voice = int(input("Select a voice (#) : "))
 
 while True:
     text = input("Text to speak (say exit to quit): ")
